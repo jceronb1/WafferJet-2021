e1bee02317d25149982730bfbcab9cb02d04a57d
simondavilas <60183163+simondavilas@users.noreply.github.com>
2021-03-03 09:54:02 -0500

    "Prueba 2.0"

--- U-Vallet/app/src/main/java/com/example/u_vallet/Activity_Registrarse.java
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_Registrarse.java
@@ -10,5 +10,6 @@ public class Activity_Registrarse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+        /* Prueba 1 */
     }
 }
