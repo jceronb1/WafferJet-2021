0f2e2d4dce71f08a363824df55ee7e791f43b078
simondavilas <60183163+simondavilas@users.noreply.github.com>
2021-03-03 11:33:30 -0500

    "Actulización interfaz de registro"

--- U-Vallet/app/src/main/java/com/example/u_vallet/Activity_Login.java
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_Login.java
@@ -2,7 +2,10 @@ package com.example.u_vallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_Login extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Activity_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button botonRegistrarse = (Button) findViewById(R.id.registrar);
+        botonRegistrarse.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegistrar = new Intent(v.getContext(), Activity_Registrarse.class);
+                startActivity(intentRegistrar);
+            }
+        }));
+
     }
 }
