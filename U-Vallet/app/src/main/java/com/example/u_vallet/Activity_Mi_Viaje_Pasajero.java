439270bc09c145800a31c25db13ecdcbfe77f11c
Gabriel Gomez Corredor <gomezga@javeriana.edu.co>
2021-03-08 10:12:14 -0500

    Interfaz mi viaje pasajero

---
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_Mi_Viaje_Pasajero.java
@@ -0,0 +1,14 @@
+package com.example.u_vallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Mi_Viaje_Pasajero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__mi__viaje__pasajero);
+    }
+}
