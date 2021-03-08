5e67528a4089f2f358d99edea0666c8468881c55
JuanDiegoCamposNeira <juand.campos@javeriana.edu.co>
2021-03-08 01:32:59 -0500

    Actividad "Explorar Viajes" creada

---
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_ExplorarViajes.java
@@ -0,0 +1,14 @@
+package com.example.u_vallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_ExplorarViajes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explorar_viajes);
+    }
+}
