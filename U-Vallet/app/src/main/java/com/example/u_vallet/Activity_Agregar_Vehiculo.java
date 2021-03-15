2320df2e067a67e1b3fffdc7c234b631cc42b881
Diego Burgos M <burgosmd@javeriana.edu.co>
2021-03-15 10:14:09 -0500

    creación de editar perfil y agregar vehiculo

---
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_Agregar_Vehiculo.java
@@ -0,0 +1,14 @@
+package com.example.u_vallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Agregar_Vehiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar__vehiculo);
+    }
+}
