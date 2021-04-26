10dceb98b8c7c3c15a165547f754e536974a269d
Gabriel Gomez Corredor <gomezga@javeriana.edu.co>
2021-04-26 14:57:25 -0500

    editar perfil avance

--- U-Vallet/app/src/main/java/com/example/u_vallet/Activity_EditarPerfil.java
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_EditarPerfil.java
@@ -1,14 +1,82 @@
 package com.example.u_vallet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class Activity_EditarPerfil extends AppCompatActivity {
+
+    int IMAGE_PICKER_REQUEST = 1;
+    int REQUEST_IMAGE_CAPTURE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
+        permisosAndroid.requestPermission(this, Manifest.permission.CAMERA, "", REQUEST_IMAGE_CAPTURE);
+        Button botonCambiarImagen = (Button) findViewById(R.id.botonCambiarImagen);
+        botonCambiarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickImage = new Intent(Intent.ACTION_PICK);
+                pickImage.setType("image/*");
+                startActivityForResult(pickImage, IMAGE_PICKER_REQUEST);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView image = (ImageView) findViewById(R.id.imagenFotoPerfil);
+        switch (requestCode) {
+            case 1: {
+                if (resultCode == RESULT_OK) {
+                    try {
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        image.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            case 2: {
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    image.setImageBitmap(imageBitmap);
+                }
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 2: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permiso Concedido", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permiso Denegado", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 }
---
+++ U-Vallet/app/src/main/java/com/example/u_vallet/permisosAndroid.java
@@ -0,0 +1,20 @@
+package com.example.u_vallet;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class permisosAndroid {
+    public static void requestPermission(Activity context, String permiso, String justificacion, int idCode) {
+        int permission = ContextCompat.checkSelfPermission(context, permiso);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)) {
+                Toast.makeText(context, "Se requiere habilitar los permisos", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permiso}, idCode);
+        }
+    }
+}
