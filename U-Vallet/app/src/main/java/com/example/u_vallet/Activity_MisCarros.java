3710f0d701216e479a8744cb6c660180084fad51
simondavilas <60183163+simondavilas@users.noreply.github.com>
2021-04-28 18:44:01 -0500

    "Visualización de las rutas en el mapa de crear viaje."

--- U-Vallet/app/src/main/java/com/example/u_vallet/Activity_MisCarros.java
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_MisCarros.java
@@ -5,12 +5,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.common.api.Api;
 
 public class Activity_MisCarros extends AppCompatActivity {
 
@@ -34,6 +37,10 @@ public class Activity_MisCarros extends AppCompatActivity {
         botonCrearViaje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*Uri gmmIntentUri = Uri.parse("http://maps.google.com/maps?saddr=4.688549,-74.050789&daddr=4.6259875,-74.0631727");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);*/
                 Intent intentCrearViaje = new Intent(v.getContext(), Activity_CrearViaje_Maps.class);
                 startActivity(intentCrearViaje);
             }
--- U-Vallet/app/src/main/java/com/example/u_vallet/DirectionsJSONParser.java
+++ U-Vallet/app/src/main/java/com/example/u_vallet/DirectionsJSONParser.java
@@ -1,5 +1,7 @@
 package com.example.u_vallet;
 
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONArray;
@@ -12,7 +14,7 @@ import java.util.List;
 
 public class DirectionsJSONParser {
     /** Receives a JSONObject and returns a list of lists containing latitude and longitude */
-    public List<List<HashMap<String,String>>> parse(JSONObject jObject){
+    /*public List<List<HashMap<String,String>>> parse(JSONObject jObject){
 
         List<List<HashMap<String, String>>> routes = new ArrayList<>() ;
         JSONArray jRoutes = null;
@@ -24,22 +26,22 @@ public class DirectionsJSONParser {
             jRoutes = jObject.getJSONArray("routes");
 
             /** Traversing all routes */
-            for(int i=0;i<jRoutes.length();i++){
+           /* for(int i=0;i<jRoutes.length();i++){
                 jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
                 List path = new ArrayList<HashMap<String, String>>();
 
                 /** Traversing all legs */
-                for(int j=0;j<jLegs.length();j++){
+                /*for(int j=0;j<jLegs.length();j++){
                     jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
 
                     /** Traversing all steps */
-                    for(int k=0;k<jSteps.length();k++){
+                   /* for(int k=0;k<jSteps.length();k++){
                         String polyline = "";
                         polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
                         List<LatLng> list = decodePoly(polyline);
 
                         /** Traversing all points */
-                        for(int l=0;l<list.size();l++){
+                     /*   for(int l=0;l<list.size();l++){
                             HashMap<String, String> hm = new HashMap<String, String>();
                             hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
                             hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
@@ -55,11 +57,115 @@ public class DirectionsJSONParser {
         }catch (Exception e){
         }
         return routes;
-    }
+    }*/
 
     /**
      * Method to decode polyline points
      * Courtesy : jeffreysambells.com/2010/05/27/decoding-polylines-from-google-maps-direction-api-with-java
+     * */
+    /*private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+        return poly;
+    }*/
+    /**
+     * Receives a JSONObject and returns a list of lists containing latitude and
+     * longitude
+     */
+    public List<List<List<HashMap<String,String>>>> parse(JSONObject jObject){
+
+        List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String,String>>>() ;
+        List<List<List<HashMap<String,String>>>> routes1 = new ArrayList<List<List<HashMap<String,String>>>>() ;
+        JSONArray jRoutes = null;
+        JSONArray jLegs = null;
+        JSONArray jSteps = null;
+        JSONObject jLegsDuration = null;
+        JSONObject jDuration = null;
+
+        try {
+
+            jRoutes = jObject.getJSONArray("routes");
+
+            /** Traversing all routes */
+            for(int i=0;i<jRoutes.length();i++){
+                jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
+                List path = new ArrayList<HashMap<String, String>>();
+                List path1 = new ArrayList<ArrayList<HashMap<String,String>>>();
+
+                // Log.d("legs",jLegs.toString());
+                /** Traversing all legs */
+                for(int j=0;j<jLegs.length();j++){
+                    jLegsDuration = (jLegs.getJSONObject(j));
+                    String duration = jLegsDuration.getJSONObject("duration").getString("text");
+                    Log.d("JSON DURATION", duration);
+                    HashMap<String,String> hmd = new HashMap<String, String>();
+                    hmd.put("dur", duration);
+                    jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
+                    // Log.d("steps",jSteps.toString());
+                    /** Traversing all steps */
+                    for(int k=0;k<jSteps.length();k++){
+
+                        String polyline = "";
+                        polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                        List<LatLng> list = decodePoly(polyline);
+                        //  Log.d("polyline",polyline.toString());
+                        /** Traversing all points */
+
+                        for(int l=0;l<list.size();l++){
+                            HashMap<String, String>    hm = new HashMap<String, String>();
+
+                            hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
+                            hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
+
+                            path.add(hm);
+                            //  Log.d("lat", Double.toString(((LatLng)list.get(l)).latitude));
+                            //  Log.d("lng", Double.toString(((LatLng)list.get(l)).longitude));
+                        }
+
+                    }
+                    path1.add(path);
+
+                }
+                routes1.add(path1);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+        }
+
+        return routes1;
+    }
+    /**
+     * Method to decode polyline points
+     * Courtesy : http://jeffreysambells.com/2010/05/27/decoding-polylines-from-google-maps-direction-api-with-java
      * */
     private List<LatLng> decodePoly(String encoded) {
 
@@ -91,6 +197,9 @@ public class DirectionsJSONParser {
                     (((double) lng / 1E5)));
             poly.add(p);
         }
+
         return poly;
     }
+
+
 }
