8962e71ce0aad261b5bd7e84b51ff7ad8b7a82ad
juanmendezp <60183261+juanmendezp@users.noreply.github.com>
2021-05-02 22:45:35 -0500

    cambiar foto de carro en editar carros

--- U-Vallet/app/src/main/java/com/example/u_vallet/Activity_EditarCarro.java
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_EditarCarro.java
@@ -1,14 +1,106 @@
 package com.example.u_vallet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.ParseException;
 
 public class Activity_EditarCarro extends AppCompatActivity {
-
+    int IMAGE_PICKER_REQUEST = 1;
+    private int STORAGE_PERMISSION_CODE = 1;
+    private Uri imagenUri;
+    private ImageView mImageView;
+    private boolean imageInclude = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_carro);
+        mImageView = (ImageView) findViewById(R.id.imagenFotoPerfil);
+        Button EditarFoto = (Button) findViewById(R.id.btn_EditarCarro_EditarFoto);
+        EditarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create and launch the new activity with an Intent
+                if(ContextCompat.checkSelfPermission( Activity_EditarCarro.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(Activity_EditarCarro.this, "You have already granted this permission!", Toast.LENGTH_SHORT).show();
+                    Intent pickImage = new Intent(Intent.ACTION_PICK);
+                    pickImage.setType("image/*");
+                    startActivityForResult(pickImage, IMAGE_PICKER_REQUEST);
+                }else{
+                    requestStoragePermission();
+                }
+            }
+        });
+        Button Guardar = (Button) findViewById(R.id.btn_EditarCarro_GuardarCambios);
+        Guardar.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Activity_MisCarros.class);
+                startActivity(intent);
+            }
+        }));
+    }
+    private void requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permiso requerido")
+                    .setMessage("Este permiso es requerido para el acceso a su galeria")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(Activity_EditarCarro.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1: {
+                if (resultCode == RESULT_OK) {
+                    try {
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        mImageView.setImageBitmap(selectedImage);
+                        imagenUri = imageUri;
+                        imageInclude = true;
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
     }
 }
