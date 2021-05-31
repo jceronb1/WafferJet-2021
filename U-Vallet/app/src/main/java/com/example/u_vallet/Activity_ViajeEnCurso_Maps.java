ed37df3aa0386fdd8ebf0d9e95f317b7b134c21f
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2021-05-31 00:34:46 -0500

    Acceder al sensor y actualizar la ubicación en firebase

--- U-Vallet/app/src/main/java/com/example/u_vallet/Activity_ViajeEnCurso_Maps.java
+++ U-Vallet/app/src/main/java/com/example/u_vallet/Activity_ViajeEnCurso_Maps.java
@@ -1,15 +1,28 @@
 package com.example.u_vallet;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,6 +31,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,74 +40,147 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Activity_ViajeEnCurso_Maps extends FragmentActivity implements OnMapReadyCallback {
 
+    //--------------------------------------------------------
+    //                         Attributes
+    //--------------------------------------------------------
+    // Map
     private GoogleMap mMap;
-
+    SupportMapFragment mapFragment;
+    // Firebase
     private FirebaseAuth mAuth;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mRef;
     private DatabaseReference mRefList;
-
+    // Firebase Path(s)
     public static final String PATH_ROUTES = "routes/";
-
+    // Hardware sensor (Accelerometer)
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TriggerEventListener triggerEventListener;
+    // Location
     private LatLng mOrigin;
     private LatLng mDestination;
+    private FusedLocationProviderClient fusedLocationClient;
+    // Button(s)
     private Button botonFinalizarViaje;
+    // Routes
     private PolylineOptions line;
+    String routeUid;
 
-    SupportMapFragment mapFragment;
-
+    //--------------------------------------------------------
+    //                         On create
+    //--------------------------------------------------------
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__viaje_en_curso__maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //------------ Firebase ------------
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance();
 
-        botonFinalizarViaje = (Button)findViewById(R.id.buttonFinalizarViaje);
-        String key = getIntent().getExtras().getString("Trip");
-        Log.d("USPRUBAF", key);
-        setRoute(key);
+        //------------ User ------------
+        routeUid = getIntent().getExtras().getString("Trip");
+        setRoute();
 
-        botonFinalizarViaje.setOnClickListener(new View.OnClickListener() {
+        //------------ Location ------------
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //------------ Accelerometer sensor ------------
+        sensorManager = (SensorManager) getSystemService(Activity_ViajeEnCurso_Maps.this.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
+        triggerEventListener = new TriggerEventListener() {
             @Override
-            public void onClick(View v) {
-                Intent finalizar = new Intent(v.getContext(), Activity_Finalizar_Viaje.class);
-                endRoute(key);
-                finalizar.putExtra("KeyEnd", key);
-                startActivity(finalizar);
+            public void onTrigger(TriggerEvent event) {
+                Log.i("SENSOR", "SE HA DETECTADO CAMBIO EN EL SENSOR");
+                // Get user's last known location
+                getUserLocation();
+                // Show the user that a change has been detected
+                Toast.makeText(Activity_ViajeEnCurso_Maps.this, "Se ha detectado un cambio en el acelerometro", Toast.LENGTH_SHORT).show();
             }
+        };
+        // Bind trigger event with sensor
+        sensorManager.requestTriggerSensor(triggerEventListener, sensor);
+
+        //------------ Buttons ------------
+        botonFinalizarViaje = findViewById(R.id.buttonFinalizarViaje);
+        botonFinalizarViaje.setOnClickListener(v -> {
+            // End the courrent route
+            endRoute();
+            // Launch new activity with finish route info
+            Intent finalizar = new Intent(v.getContext(), Activity_Finalizar_Viaje.class);
+            finalizar.putExtra("KeyEnd", routeUid);
+            startActivity(finalizar);
         });
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    //--------------------------------------------------------
+    //                         Methods
+    //--------------------------------------------------------
+    private void getUserLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            updateOriginLocationFirebase(location);
+                        }
+                    }
+                });
+    }
+
+    private void updateOriginLocationFirebase(Location location) {
+        // Firebase reference
+        mRef = mDatabase.getReference(PATH_ROUTES).child(routeUid).child("originLocation");
+        // Create HashMap to store location
+        HashMap<String, Double> newLocation = new HashMap<String, Double>();
+        newLocation.put("latitude", location.getLatitude());
+        newLocation.put("longitude", location.getLongitude());
+        // Set new location in firebase
+        mRef.setValue(newLocation);
+    }
+
+    //--------------------------------------------------------
+    //                          Map
+    //--------------------------------------------------------
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
     }
-    private void endRoute(String key){
-        mRef = mDatabase.getReference(PATH_ROUTES).child(key);
+
+    //--------------------------------------------------------
+    //                          Routes
+    //--------------------------------------------------------
+    private void endRoute(){
+        mRef = mDatabase.getReference(PATH_ROUTES).child(routeUid);
         mRef.child("status").setValue("finished");
     }
-    private void setRoute(String key){
-        mRef = mDatabase.getReference(PATH_ROUTES).child(key);
+
+    private void setRoute(){
+        //----------------- Listener to origin location changes -----------------
+        mRef = mDatabase.getReference(PATH_ROUTES).child(routeUid);
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
@@ -107,8 +194,6 @@ public class Activity_ViajeEnCurso_Maps extends FragmentActivity implements OnMa
                 mOrigin = new LatLng(originLat,originLong);
                 mDestination = new LatLng(destinationLat, destinationLong);
 
-                Log.d("USPRUEBA", keyF+ "/"+key );
-
                 mMap.addMarker(new MarkerOptions().position(mOrigin).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
                 mMap.addMarker(new MarkerOptions().position(mDestination));
                 mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
@@ -119,7 +204,9 @@ public class Activity_ViajeEnCurso_Maps extends FragmentActivity implements OnMa
 
             }
         });
-        mRefList = mDatabase.getReference(PATH_ROUTES).child(key).child("route");
+
+        //----------------- Draw the routes line -----------------
+        mRefList = mDatabase.getReference(PATH_ROUTES).child(routeUid).child("route");
         mRefList.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -146,4 +233,5 @@ public class Activity_ViajeEnCurso_Maps extends FragmentActivity implements OnMa
             }
         });
     }
+
 }
